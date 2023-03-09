9cedbd39cdcb9394eecf264768ba4b39ca5461b0
Matthew Hanson <dingbat91@gmail.com>
2023-03-09 14:23:40 +0000

    exercise 2 complete !

--- exercise002.js
+++ exercise002.js
@@ -2,11 +2,20 @@ import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 
-const fetchData = (apiEndPoint) => {
-  fetch(apiEndPoint)
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-    .catch((error) => console.log(error));
+//You'll have to grab an api key from superheroapi.com and throw it into the variable here.
+const apiKey = //!REPLACE THIS COMMENT WITH YOUR API KEY
+const superheroAPI = `https://superheroapi.com/api/${apiKey}/620`;
+
+const fetchData = async (apiEndPoint) => {
+	try {
+		const data = await (await fetch(jsonTypicode)).json();
+		const superherodata = await (await fetch(superheroAPI)).json();
+		console.log(data);
+    //should display Spiderman
+		console.log(superherodata.name);
+	} catch (error) {
+		console.log(e);
+	}
 };
 
 fetchData(jsonTypicode);
